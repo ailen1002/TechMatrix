33533a35fae53d97bf358997cc8e329b483f7c8d
Andrew <ailen1002@163.com>
2024-10-17 12:01:24 +0800

    新建LoginViewModel

---
+++ AvoTech/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Reactive;
+using ReactiveUI;
+
+namespace AvoTech.ViewModels;
+
+public class LoginViewModel : ViewModelBase
+{
+    public ReactiveCommand<Unit, Unit> OpenRegisterCommand { get; }
+    
+    public LoginViewModel()
+    {
+        OpenRegisterCommand = ReactiveCommand.Create(Register);
+    }
+    
+    private void Register()
+    {
+        // Todo 制作注册页面
+    }
+}
