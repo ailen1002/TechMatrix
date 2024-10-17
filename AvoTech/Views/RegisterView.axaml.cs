bd5c8fa5b181e29989bdaf401601ee65defd1d1a
Andrew <ailen1002@163.com>
2024-10-17 14:48:02 +0800

    新增RegisterView和RegisterViewModel

---
+++ AvoTech/Views/RegisterView.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvoTech.Views;
+
+public partial class RegisterView : Window
+{
+    public RegisterView()
+    {
+        InitializeComponent();
+        
+    }
+}
