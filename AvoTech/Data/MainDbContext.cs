8a3062e135a00765c11cc49752e97d43aa83a245
zhangxu <ailen1002@163.com>
2024-10-30 15:50:36 +0800

    创建数据库，及对应数据模型的表。

--- AvoTech/Data/MainDbContext.cs
+++ AvoTech/Data/MainDbContext.cs
@@ -1,6 +1,39 @@
-﻿namespace AvoTech.Data;
+﻿using System;
+using AvoTech.Models;
+using Microsoft.EntityFrameworkCore;
+using NLog;
 
-public class MainDbContext
+namespace AvoTech.Data;
+
+public class MainDbContext : DbContext
 {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
     
+    public DbSet<User> Users { get; set; }
+    
+    public MainDbContext(DbContextOptions<MainDbContext> options) : base(options)
+    {
+        EnsureDatabaseCreated();
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .Property(u => u.UserName)
+            .HasMaxLength(64);
+    }
+    
+    // 确保数据库和表已创建
+    public void EnsureDatabaseCreated()
+    {
+        try
+        {
+            var isCreated = Database.EnsureCreated();
+            Logger.Info(isCreated ? "Database created." : "Database already exists.");
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex, "Error ensuring database creation.");
+        }
+    }
 }
